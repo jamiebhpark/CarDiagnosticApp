2e31934583fe17eb7c7a737f975805ca419c4907
JAMIEPARK90 <jamiebhpark@gmail.com>
2024-11-05 15:55:26 +0900

    Initial Commit

---
+++ CarDiagnosticApp/CarDiagnosticAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarDiagnosticAppApp.swift
+//  CarDiagnosticApp
+//
+//  Created by JONGHUN PARK on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct CarDiagnosticAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarDiagnosticApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CarDiagnosticApp
+//
+//  Created by JONGHUN PARK on 11/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
